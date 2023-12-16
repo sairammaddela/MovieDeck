06b742388882633096e821dd3acf08cff0494439
unknown <sairammaddela2@gmail.com>
2023-12-16 13:52:35 +0530

    moviedeckcommit1216

--- index.js
+++ index.js
@@ -1,4 +1,85 @@
 const movieblc=document.getElementById("movieblock");
+let page=1;
+const previous_page=document.getElementById("prev");
+const pagevalue=document.getElementById("page_value");
+const next_page=document.getElementById("next");
+let rating_bool=true;
+let date_bool=false;
+let search_bool=false;
+let totalpages;
+let search_item;
+//let currentfunction;
+if(page===1)
+{
+    previous_page.style.display="none";
+}
+pagevalue.innerHTML=`${page}`;
+function nextclick(e)
+{
+    page++;
+    //currentfunction();
+    pagevalue.innerHTML=`${page}`;
+    if(page===totalpages)
+    {
+        next_page.style.display="none";
+    }
+    if(page!==1)
+    {
+        previous_page.style.display="block";
+    }
+    if(rating_bool)
+    {
+        handlepromise();
+    }
+    else if(date_bool)
+    {
+        sortbydate();
+    }
+    else if(search_bool)
+    {
+        searchmovie();
+    }
+}
+function prevclick()
+{
+    page--;
+    pagevalue.innerHTML=`${page}`;
+    if(page===1)
+    {
+        previous_page.style.display="none";
+    }
+    if(page<totalpages)
+    {
+        next_page.style.display="block";
+    }
+    if(rating_bool)
+    {
+        handlepromise();
+    }
+    else if(date_bool)
+    {
+        sortbydate();
+    }
+    else if(search_bool)
+    {
+        searchmovie();
+    }
+}
+function handleallbtn()
+{
+    if(rating_bool)
+    {
+        handlepromise();
+    }
+    else if(date_bool)
+    {
+        sortbydate();
+    }
+    else if(search_bool)
+    {
+        searchmovie();
+    }
+}
 function card(poster,rating,vote_count,id,favv)
 {
     const newdiv=document.createElement("div");
@@ -42,16 +123,28 @@ function favfunc(event)
         favarr.splice(rem,1);
     }
 }
+function reset()
+{
+    pagevalue.innerHTML=`${page}`;
+    previous_page.style.display="none";
+    next_page.style.display="block";
+}
 async function handlepromise()
 {
+    if(!rating_bool)
+    {
+        page=1;
+        reset();
+    }
     while(movieblc.firstChild)
     {
         movieblc.removeChild(movieblc.firstChild);
     }
-    const res=await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=f531333d637d0c44abc85b3e74db2186&language=en-US&page=1");
+    const res=await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=f531333d637d0c44abc85b3e74db2186&language=en-US&page=${page}`);
     const results=await res.json();
     console.log(favarr);
     const movielist=results.results;
+    totalpages=results.total_pages;
     console.log(movielist,"hey");
     movielist.forEach(element => {
         //console.log(favarr.indexOf(element.id+''));
@@ -63,6 +156,9 @@ async function handlepromise()
         card("https://image.tmdb.org/t/p/original/"+element.poster_path,element.vote_average,element.vote_count,element.id,"fa-regular fa-heart fa-xl");
         
     });
+    date_bool=false;
+    rating_bool=true;
+    search_bool=false;
 }
 handlepromise();
 function favoriteHandler(e)
@@ -80,14 +176,20 @@ function favoriteHandler(e)
         })
     })
 }
-function sortbydate(event)
+const accesstoken="Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDA4ZGE4YTE2NTZkNjNmZmYyZjQwZTQwZTNiMmJiNSIsInN1YiI6IjY1MDQwMDUxNmEyMjI3MDBjM2I3ZGZlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.85r15wx6lm6UFbM8UB0J12Uwv8_L6eychwHX6ue6Op0";
+const headers={
+    Authorization:accesstoken,
+    accept: "application/json"
+}
+function sortbydate()
 {
-    const accesstoken="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDA4ZGE4YTE2NTZkNjNmZmYyZjQwZTQwZTNiMmJiNSIsInN1YiI6IjY1MDQwMDUxNmEyMjI3MDBjM2I3ZGZlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.85r15wx6lm6UFbM8UB0J12Uwv8_L6eychwHX6ue6Op0";
-    const url="https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
-    const headers={
-        Authorization:' Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDA4ZGE4YTE2NTZkNjNmZmYyZjQwZTQwZTNiMmJiNSIsInN1YiI6IjY1MDQwMDUxNmEyMjI3MDBjM2I3ZGZlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.85r15wx6lm6UFbM8UB0J12Uwv8_L6eychwHX6ue6Op0',
-        accept: "application/json"
+    if(!date_bool)
+    {
+        page=1;
+        reset();
     }
+    const url=`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}`;
+    
     while(movieblc.firstChild)
     {
         movieblc.removeChild(movieblc.firstChild);
@@ -103,9 +205,58 @@ function sortbydate(event)
                 }
                 else
                     card("https://image.tmdb.org/t/p/original/"+val.poster_path,val.vote_average,val.vote_count,val.id,"fa-regular fa-heart fa-xl");
-            })
+            });
+            totalpages=data1.total_pages;
         })
-    })
+    });
+    date_bool=true;
+    rating_bool=false;
+    search_bool=false;
+}
+const debounce=debouncing(searchmovie,500);
+function debouncing(func,delay)
+{
+    let timer;
+    return function(e)
+    {
+        clearTimeout(timer);
+            timer=setTimeout(()=>{
+                func(e);
+            },delay);
+    }
+}
+function searchmovie(e)
+{
+    if(!search_bool)
+    {
+        page=1;
+        reset();
+    }
+    if(e)
+    {
+    search_item=e.target.value;
+    }
+    let url=`https://api.themoviedb.org/3/search/movie?query=${search_item}&page=${page}`;
+    while(movieblc.firstChild)
+    {
+        movieblc.removeChild(movieblc.firstChild);
+    }
+    fetch(url,{headers}).then(data=>{
+        data.json().then(data1=>{
+            data1.results.forEach(val=>{
+                if(favarr.indexOf(val.id+'')>-1)
+                {
+                    card("https://image.tmdb.org/t/p/original/"+val.poster_path,val.vote_average,val.vote_count,val.id,"fa-solid fa-heart fa-xl");
+                }
+                else
+                    card("https://image.tmdb.org/t/p/original/"+val.poster_path,val.vote_average,val.vote_count,val.id,"fa-regular fa-heart fa-xl");
+            });
+            totalpages=data1.total_pages;
+        })
+    });
+    date_bool=false;
+    rating_bool=false;
+    search_bool=true;
 
 }
 
